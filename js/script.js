d12d5ca9c25203ee9ab901e89660d0a8a686e3d7
Jess247 <jesss8692@hotmail.com>
2025-12-23 13:33:49 +0100

    Add project files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const textEl = document.getElementById('text')
+const dateEl =  document.getElementById('date')
+const bodyEl = document.getElementById('body')
+
+const date = new Date
+const fullDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+const imgUrl =  `https://www.sourcesplash.com/i/random?w=1920&h=1080&q=ocean`
+dateEl.textContent =  fullDate
+
+bodyEl.style.backgroundImage = `url(${imgUrl})`
+
+// get the next content 
+fetch('../data/affirmations_and_love_notes.json')
+.then(res => res.json())
+.then(data => {
+    const {affirmations, loveNotes} = data;
+
+    function gerRandomNote() {
+        const randomAffirmation =  affirmations[Math.floor(Math.random() * affirmations.length)]
+        const randomLoveNote = loveNotes[Math.floor(Math.random() * loveNotes.length)]
+
+        const isAffirmation = Math.random() > 0.5
+
+        return isAffirmation ? randomAffirmation : randomLoveNote
+    }
+    textEl.textContent = gerRandomNote()
+})
+.catch(error => console.error('Error loading JSON: ', error))
