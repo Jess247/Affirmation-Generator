9f0bef7510a73b6116ac4bc21e4b0cfc2e3800ff
Jess247 <jesss8692@hotmail.com>
2026-01-02 11:38:49 +0100

    delete back ground

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ const dateEl =  document.getElementById('date')
 const bodyEl = document.getElementById('body')
 
 const date = new Date
-const fullDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
-const imgUrl =  `https://www.sourcesplash.com/i/random?w=1920&h=1080&q=universe`
+const fullDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+
 dateEl.textContent =  fullDate
 
-bodyEl.style.backgroundImage = `url(${imgUrl})`
 
 // get the next content 
 fetch('../data/affirmations_and_love_notes.json')
